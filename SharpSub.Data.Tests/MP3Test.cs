293aac43cd864c3bdaec2a7e1dff2e4f97c05584
bmj1086 <dumbestcrayon@gmail.com>
2011-06-18 11:51:01 -0500

    small change to Unit test

--- SharpSub.Data.Tests/MP3Test.cs
+++ SharpSub.Data.Tests/MP3Test.cs
@@ -75,10 +75,11 @@ namespace SharpSub.Data.Tests
             string username = "Guest";
             string password = "notbrett";
             SubsonicRequest.Login(serverURL, username, password);
+            
             Random random = new Random();
-
             var artistList = SubsonicRequest.GetArtistList();
             var artist = artistList[random.Next(artistList.Count)];
+            //var artist = SubsonicRequest.GetArtistList().First();
 
             Album album = SubsonicRequest.GetArtistAlbums(artist).First();
             Song song = SubsonicRequest.GetAlbumSongs(album.ID).First();
